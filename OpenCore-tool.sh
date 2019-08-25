f853479290d51b00e01c8e093e534967bdc0013c
rusty-bits <rusty_bits@aol.com>
2019-08-25 08:15:36 -0400

    fix release build

--- OpenCore-tool.sh
+++ OpenCore-tool.sh
@@ -4,6 +4,7 @@ set -e
 shopt -s extglob
 
 BASE_DIR=`pwd`
+RES_DIR="$BASE_DIR/resources"
 
 LOGFILE="tool.log"
 
@@ -27,9 +28,9 @@ set_up_dirs() {
 	fi
 
 	echo -e -n "Creating $BUILD_DIR directory ... " >$(tty)
-	mkdir -p $BASE_DIR/$BUILD_DIR/BOOT
-	mkdir -p $BASE_DIR/$BUILD_DIR/OC/Kexts
-	mkdir -p $BASE_DIR/$BUILD_DIR/OC/Drivers
+	mkdir -p $BUILD_DIR/BOOT
+	mkdir -p $BUILD_DIR/OC/Kexts
+	mkdir -p $BUILD_DIR/OC/Drivers
 	fin
 }
 
@@ -41,7 +42,7 @@ check_updates() {
 }
 
 build_drivers() {
-	driver_list=() #have this read drivers from conf|g.plist
+	driver_list=() #todo-have this read drivers from config.plist
 	built=()
 	while IFS= read -r line; do
 		driver_list+=("$line")
@@ -50,7 +51,7 @@ build_drivers() {
 	echo $1 $driver_list >$(tty)
 
 	echo -e -n "Making BaseTools ... " >$(tty)
-	cd $BASE_DIR/resources/UDK
+	cd $RES_DIR/UDK
 	source edksetup.sh --reconfig
 	make -C BaseTools; fin
 
@@ -72,7 +73,7 @@ build_drivers() {
 		if [ "$1" == "$BASE_DIR/config-$AUDK_CONFIG" ]; then
 			driver_name=`echo $driver|rev|cut -f 1 -d/|rev`
 			echo -e -n "Copying $driver_name into $BUILD_DIR/OC/Drivers ... " >$(tty)
-			cp $BASE_DIR/resources/UDK/Build/$driver_pkg/$AUDK_BUILD_DIR/X64/$driver_name $BASE_DIR/$BUILD_DIR/OC/Drivers; fin
+			cp $RES_DIR/UDK/Build/$driver_pkg/$AUDK_BUILD_DIR/X64/$driver_name $BUILD_DIR/OC/Drivers; fin
 		fi
 	done
 }
@@ -82,48 +83,47 @@ check_base() {
 		echo -e -n "Cloning acidanthera/audk into UDK ... " >$(tty)
 		git clone https://github.com/acidanthera/audk resources/UDK; fin
 	fi
-#	CONFIG_DIR="$BASE_DIR/Docs/base"
 	build_drivers "$BASE_DIR/Docs/base"
 
 	echo -e -n "Copying BOOTx64.efi into $BUILD_DIR/BOOT ... " >$(tty)
-	cp $BASE_DIR/resources/UDK/Build/OpenCorePkg/$AUDK_BUILD_DIR/X64/BOOTx64.efi $BASE_DIR/$BUILD_DIR/BOOT
+	cp $RES_DIR/UDK/Build/OpenCorePkg/$AUDK_BUILD_DIR/X64/BOOTx64.efi $BUILD_DIR/BOOT
 	fin
 
 	echo -e -n "Copying OpenCore.efi into $BUILD_DIR/OC ... " >$(tty)
-	cp $BASE_DIR/resources/UDK/Build/OpenCorePkg/$AUDK_BUILD_DIR/X64/OpenCore.efi $BASE_DIR/$BUILD_DIR/OC
+	cp $RES_DIR/UDK/Build/OpenCorePkg/$AUDK_BUILD_DIR/X64/OpenCore.efi $BUILD_DIR/OC
 	fin
 }
 
 config_changed() {
-	cp $BASE_DIR/resources/UDK/OpenCorePkg/Docs/Sample.plist $BASE_DIR/Docs/Sample.plist
-	cp $BASE_DIR/resources/UDK/OpenCorePkg/Docs/SampleFull.plist $BASE_DIR/Docs/SampleFull.plist
+	cp $RES_DIR/UDK/OpenCorePkg/Docs/Sample.plist $BASE_DIR/Docs/Sample.plist
+	cp $RES_DIR/UDK/OpenCorePkg/Docs/SampleFull.plist $BASE_DIR/Docs/SampleFull.plist
 	echo -e "\n${YELLOW}WARNING:${NC} Sample.plist & SampleFull.plist have been updated\n${RED}Make sure${NC} $BASE_DIR/$CONFIG_PLIST ${RED}is up to date${NC}.\nRun the tool again if you make any changes." >$(tty)
 }
 
 check_config() {
 	if [ -f "$BASE_DIR/$CONFIG_PLIST" ]; then
 		echo -e -n "Copying $CONFIG_PLIST into $BUILD_DIR/OC ... " >$(tty)
-		cp $BASE_DIR/$CONFIG_PLIST $BASE_DIR/$BUILD_DIR/OC/config.plist
+		cp $BASE_DIR/$CONFIG_PLIST $BUILD_DIR/OC/config.plist
 		fin
 	else
 		echo -e "\n${RED}ERROR: ${NC}$BASE_DIR/$CONFIG_PLIST does not exist\n\nPlease create this file and run the tool again." >$(tty)
 		exit 1
 	fi
-	cmp --silent $BASE_DIR/resources/UDK/OpenCorePkg/Docs/Sample.plist $BASE_DIR/Docs/Sample.plist || config_changed
-	cmp --silent $BASE_DIR/resources/UDK/OpenCorePkg/Docs/SampleFull.plist $BASE_DIR/Docs/SampleFull.plist || config_changed
+	cmp --silent $RES_DIR/UDK/OpenCorePkg/Docs/Sample.plist $BASE_DIR/Docs/Sample.plist || config_changed
+	cmp --silent $RES_DIR/UDK/OpenCorePkg/Docs/SampleFull.plist $BASE_DIR/Docs/SampleFull.plist || config_changed
 }
 
 build_vault() {
 	use_vault=`/usr/libexec/PlistBuddy -c "print :Misc:Security:RequireVault" $BASE_DIR/$CONFIG_PLIST`||use_vault="false"
 	if [ "$use_vault" == "true" ]; then
 		echo -e -n "\nBuilding vault files for $BUILD_DIR ... " >$(tty)
-		cd $BASE_DIR/$BUILD_DIR/OC
+		cd $BUILD_DIR/OC
 		if ls vault* 1> /dev/null 2>&1; then
 			rm vault.*
 		fi
-		$BASE_DIR/resources/UDK/OcSupportPkg/Utilities/CreateVault/create_vault.sh .
-		make -C $BASE_DIR/resources/UDK/OcSupportPkg/Utilities/RsaTool
-		$BASE_DIR/resources/UDK/OcSupportPkg/Utilities/RsaTool/RsaTool -sign vault.plist vault.sig vault.pub
+		$RES_DIR/UDK/OcSupportPkg/Utilities/CreateVault/create_vault.sh .
+		make -C $RES_DIR/UDK/OcSupportPkg/Utilities/RsaTool
+		$RES_DIR/UDK/OcSupportPkg/Utilities/RsaTool/RsaTool -sign vault.plist vault.sig vault.pub
 		off=$(($(strings -a -t d OpenCore.efi | grep "=BEGIN OC VAULT=" | cut -f1 -d' ')+16))
 		dd of=OpenCore.efi if=vault.pub bs=1 seek=$off count=520 conv=notrunc
 		rm vault.pub
@@ -137,7 +137,7 @@ set_build_type() {
 	case $ARG2 in
 		d?(ebug) )
 			echo -e "\n${GREEN}Setting up ${YELLOW}DEBUG${GREEN} environment${NC}" >$(tty)
-			BUILD_DIR="EFI-debug"
+			BUILD_DIR="$BASE_DIR/EFI-debug"
 			XCODE_CONFIG="Debug"
 			AUDK_CONFIG="DEBUG"
 			AUDK_BUILD_DIR="DEBUG_XCODE5"
@@ -145,7 +145,7 @@ set_build_type() {
 			;;
 		r?(elease) )
 			echo -e "\n${GREEN}Setting up ${YELLOW}RELEASE${GREEN} environment${NC}" >$(tty)
-			BUILD_DIR="EFI-release"
+			BUILD_DIR="$BASE_DIR/EFI-release"
 			XCODE_CONFIG="Release"
 			AUDK_CONFIG="RELEASE"
 			AUDK_BUILD_DIR="RELEASE_XCODE5"
@@ -160,21 +160,21 @@ set_build_type() {
 
 link_lilu() {
 	if [ ! -L "Lilu.kext" ]; then
-		ln -s $BASE_DIR/resources/Kext_builds/Lilu/build/Debug/Lilu.kext .
+		ln -s $RES_DIR/Kext_builds/Lilu/build/Debug/Lilu.kext .
 	fi
 }
 
 build_kexts() {
-	kext_list=() #read these from config.plist
+	kext_list=() #todo-read these from config.plist
 	while IFS= read -r line; do
 		kext_list+=("$line")
 	done < "$BASE_DIR/config-$AUDK_CONFIG/kext.list"
-	if [ ! -d "$BASE_DIR/resources/Kext_builds" ]; then
-		mkdir $BASE_DIR/resources/Kext_builds
+	if [ ! -d "$RES_DIR/Kext_builds" ]; then
+		mkdir $RES_DIR/Kext_builds
 	fi
 	for kext in "${kext_list[@]}"
 	do
-		cd $BASE_DIR/resources/Kext_builds
+		cd $RES_DIR/Kext_builds
 		git_url=`echo $kext|rev|cut -f 2- -d /|rev`
 		kext_name=`echo $kext|rev|cut -f 1 -d /|rev`
 		kext_pkg=`echo $git_url|rev|cut -f 1 -d /|rev`
@@ -191,7 +191,7 @@ build_kexts() {
 		xcodebuild -config $XCODE_CONFIG build
 		fin
 		echo -e -n "Copying $kext_name into $BUILD_DIR/OC/Kexts ... " >$(tty)
-		cp -r $BASE_DIR/resources/Kext_builds/$kext_pkg/build/$XCODE_CONFIG/$kext_name $BASE_DIR/$BUILD_DIR/OC/Kexts
+		cp -r $RES_DIR/Kext_builds/$kext_pkg/build/$XCODE_CONFIG/$kext_name $BUILD_DIR/OC/Kexts
 		fin
 	done
 }
@@ -247,5 +247,5 @@ exec 1>&6 6>&- 2>&1 #stop logfile
 echo -e "\n${GREEN}Finished building ${YELLOW}$BUILD_DIR${NC}"
 
 echo -e "\n${GREEN}Any git updates will appear below ...${NC}"
-cat $BASE_DIR/$LOGFILE | grep github || echo -e "No git updates were found"
+cat $BASE_DIR/$LOGFILE|grep "From http"|| echo -e "No git updates were found"
 fin
