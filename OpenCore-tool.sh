03d20fe59d922242c423f9d936418c36b56790a2
rusty-bits <rusty_bits@aol.com>
2019-08-29 13:21:30 -0400

    basic tool directory handling added

--- OpenCore-tool.sh
+++ OpenCore-tool.sh
@@ -3,121 +3,134 @@
 set -e
 shopt -s extglob
 
-res_list=( \
-	"0.base" "https://github.com/acidanthera/EfiPkg" "" \
-	"0.base" "https://github.com/acidanthera/MacInfoPkg" "" \
-	"0.base" "https://github.com/acidanthera/OcSupportPkg" "" \
-	"BOOTx64.efi" "https://github.com/acidanthera/OpenCorePkg" "BOOT" \
-	"OpenCore.efi" "https://github.com/acidanthera/OpenCorePkg" "OC" \
-	)
-
 BASE_DIR=`pwd`
 RES_DIR="$BASE_DIR/resources"
 BASE_TOOLS="unbuilt"
 
 LOGFILE="tool.log"
 
-ARG1=$1
-ARG2=$2
+ARG1=$1; ARG2=$2
 
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+print_resources() {
+	for (( i = 0; i < ${#res_list[@]} ; i+=4 )); do
+		echo -e "${res_list[i]}\t${res_list[i+1]}\t${res_list[i+2]}\t${res_list[i+3]}" >$(tty)
+	done
+}
+
+msg() {
+	echo -e -n "$1" >$(tty)
+}
+
 fin() {
 	echo -e "${GREEN}done${NC}" >$(tty)
 }
 
-set_up_dirs() {
-	cd $BASE_DIR
+set_up_dest_dir() {
 	if [ -d "$BUILD_DIR" ]; then
-		echo -e -n "Removing old $BUILD_DIR ... " >$(tty)
-		rm -Rf $BUILD_DIR; fin
+		msg "Removing old $BUILD_DIR ... "
+		rm -rf $BUILD_DIR; fin
 	fi
-
-	echo -e -n "Creating new $BUILD_DIR ... " >$(tty)
+	msg "Creating new $BUILD_DIR ... "
 	mkdir -p $BUILD_DIR/BOOT
-	mkdir -p $BUILD_DIR/OC/Kexts
-	mkdir -p $BUILD_DIR/OC/Drivers
+	mkdir -p $BUILD_DIR/OC
 	fin
 }
 
+init_res_list() {
+	res_list=( \
+		"0.base" "https://github.com/acidanthera/EfiPkg" "" "" \
+		"0.base" "https://github.com/acidanthera/MacInfoPkg" "" "" \
+		"0.base" "https://github.com/acidanthera/OcSupportPkg" "" "" \
+		"BOOTx64.efi" "https://github.com/acidanthera/OpenCorePkg" "" "BOOT" \
+		"OpenCore.efi" "https://github.com/acidanthera/OpenCorePkg" "" "OC" \
+		"config.plist" "base/config" "$BASE_DIR/$AUDK_CONFIG" "OC"
+		)
+	}
+
+clone() {
+	if [ ! -d "$1" ]; then
+		if [ "1" == "dummyPkg" ]; then
+			mkdir dummyPkg
+		else
+			msg "Cloning $2 ..."
+			git clone $2; fin
+		fi
+		echo "new" > $1/gitStatDEBUG
+		echo "new" > $1/gitStatRELEASE
+	fi
+}
+
 missing() {
-	echo -e "\n${RED}ERROR:${NC} $1 not found, install it to continue"
+	msg "\n${RED}ERROR:${NC} $1 not found, install it to continue\n"
 	exit 1
 }
 
 check_requirements() {
-	echo -e -n "\nChecking if required tools and files exist ..." >$(tty)
+	msg "\nChecking if required tools and files exist ..."
 	if [ ! -f "$BASE_DIR/$CONFIG_PLIST" ]; then
-		echo -e "\n${RED}ERROR: ${NC}$BASE_DIR/$CONFIG_PLIST does not exist\n\nPlease create this file and run the tool again." >$(tty)
+		msg "\n${RED}ERROR: ${NC}$BASE_DIR/$CONFIG_PLIST does not exist\n\nPlease create this file and run the tool again.\n"
 		exit 1
 	fi
 	which xcodebuild||missing "xcodebuild"
 	which nasm||missing "nasm"
 	which mtoc||missing "mtoc"
+	which git||missing "git"
 	fin
 }
 
 check_for_updates() {
-	echo -e -n "\nChecking $BASE_DIR for git updates ... " >$(tty)
+	msg "\nChecking for updates ... "
 	cd $BASE_DIR
 	find . -maxdepth 4 -name .git -type d|rev|cut -c 6-|rev|xargs -I {} git -C {} pull
 	fin
 }
 
 build_shell_tool() {
-	echo -e "\n${GREEN}Setting up OpenCoreShell environment${NC}" >$(tty)
 	if [ ! -d "$BASE_DIR/resources" ]; then
 		mkdir $BASE_DIR/resources
 	fi
 	cd $BASE_DIR/resources
-	if [ ! -d "OpenCoreShell" ]; then
-		echo -e -n "Cloning OpenCoreShell ... " >$(tty)
-		git clone https://github.com/acidanthera/OpenCoreShell
-		fin
-	fi
+	clone "OpenCoreShell" "https//github.com/acidanthera/OpenCoreShell"
 	cd OpenCoreShell
 	if [ ! -d "UDK" ]; then
-		echo -e -n "Cloning UDK2018 ... " >$(tty)
-#		git clone https://github.com/tianocore/edk2 -b UDK2018 --depth=1 UDK
-		git clone https://github.com/acidanthera/audk UDK
+		msg "Cloning UDK2018 ... "
+		git clone https://github.com/tianocore/edk2 -b UDK2018 --depth=1 UDK
 		fin
 	fi
 	cd UDK
-	echo -e -n "Making UDK2018 BaseTools ... " >$(tty)
+	msg "Making UDK2018 BaseTools ... "
 	unset WORKSPACE
 	unset EDK_TOOLS_PATH
 	source edksetup.sh --reconfig
 	make -C BaseTools
 	fin
 
-	echo -e -n "Patching UDK2018 ... " >$(tty)
-	for i in ../Patches/* ; do
-		git apply "$i" || echo "$i ignored"
+	msg "Patching UDK2018 ... "
+	for p in ../Patches/* ; do
+		git apply "$p"||echo "$p ignored, does not apply or alread done"
 	done
 	fin
 
-	echo -e -n "Building Shell.efi (OpenCoreShell.efi) ... " >$(tty)
+	msg "Building Shell.efi (OpenCoreShell.efi) ... "
 	build -a X64 -b DEBUG -t XCODE5 -p ShellPkg/ShellPkg.dsc
 	fin
+	srce="`pwd`/Build/Shell/DEBUG_XCODE5/X64"; dest="$BASE_DIR/extras"
+	mkdir -p $dest && cp $srce/Shell.efi $_
 }
 
 build_kext() {
 	cd $RES_DIR/Kext_builds
 	git_url=${res_list[$1+1]}
 	pkg_name=`echo $git_url|rev|cut -f 1 -d /|rev`
-	if [ ! -d "$pkg_name" ]; then
-		echo -e -n "Cloning $git_url ..." >$(tty)
-		git clone $git_url
-		echo "new" > $pkg_name/gitStatDEBUG
-		echo "new" > $pkg_name/gitStatRELEASE
-		fin
-	fi
+	clone "$pkg_name" "$git_url"
 	cd $pkg_name
 	if [ "`git rev-parse HEAD`" != "`cat gitStat$AUDK_CONFIG`" ]; then
-		echo -e -n "Building $pkg_name ... " >$(tty)
+		msg "Building $pkg_name ... "
 		if [ "$pkg_name" != "Lilu" ]; then
 			if [ ! -L "Lilu.kext" ]; then
 				ln -s $RES_DIR/Kext_builds/Lilu/build/Debug/Lilu.kext .
@@ -146,21 +159,14 @@ build_driver() {
 	cd $RES_DIR/UDK
 	git_url=${res_list[$1+1]}
 	pkg_name=`echo $git_url|rev|cut -f 1 -d /|rev`
-	if [ "$pkg_name" == "dummyPkg" ]; then
-		make_dummy_pkg
-	elif [ ! -d "$pkg_name" ]; then
-		echo -e -n "Cloning $git_url ... " >$(tty)
-		git clone $git_url; fin
-		echo "new" > $pkg_name/gitStatDEBUG
-		echo "new" > $pkg_name/gitStatRELEASE
-	fi
+	clone $pkg_name $git_url
 	if [[ ! " ${built[@]} " =~ " ${pkg_name} " ]]; then
 		cd $pkg_name
 		built+=("$pkg_name")
 		if [ -f "$pkg_name.dsc" ]; then
 			if [ "`git rev-parse HEAD`" != "`cat gitStat$AUDK_CONFIG`" ]; then
 				if [ "$BASE_TOOLS" == "unbuilt" ]; then
-					echo -e -n "Making base tools ... " >$(tty)
+					msg "Making base tools ... "
 					cd ..
 					source edksetup.sh --reconfig
 					make -C BaseTools; fin
@@ -168,7 +174,7 @@ build_driver() {
 					BASE_TOOLS="built"
 				fi
 				cd ..
-				echo -e -n "Building $pkg_name ... " >$(tty)
+				msg "Building $pkg_name ... "
 				build -a X64 -b $AUDK_CONFIG -t XCODE5 -p $pkg_name/$pkg_name.dsc
 				cd $pkg_name
 				git rev-parse HEAD > gitStat$AUDK_CONFIG
@@ -179,81 +185,68 @@ build_driver() {
 }
 
 build_resources() {
+	cd $BASE_DIR
 	built=()
-	echo -e "\n${GREEN}Building resources${NC}" >$(tty)
+	msg "\n${GREEN}Building needed resources${NC}\n"
 
-	if [ ! -d "resources/UDK" ];then
-		echo -e -n "Cloning acidanthera/audk into UDK ... " >$(tty)
-		git clone https://github.com/acidanthera/audk resources/UDK; fin
-	fi
+	clone "resources/UDK" "https://github.com/acidanthera/audk resources/UDK"
 
 	if [ ! -d "$RES_DIR/Kext_builds" ]; then
 		mkdir $RES_DIR/Kext_builds
 	fi
 
-
-	for (( i = 0; i < ${#res_list[@]} ; i+=3 )); do
+	for (( i = 0; i < ${#res_list[@]} ; i+=4 )); do
 		case `echo ${res_list[i]}|rev|cut -f 1 -d .|rev` in
 			"base" | "efi" )
-				build_driver "$i"
+				if [ "${res_list[i]}" == "Shell.efi" ]; then #special case
+					build_shell_tool
+					res_list[i+2]=$dest
+				else
+					build_driver "$i"
+					res_list[i+2]="$RES_DIR/UDK/Build/$pkg_name/$AUDK_BUILD_DIR/X64"
+				fi
 				;;
 			"kext" )
 				build_kext "$i"
-				;;
-			"tool" )
-				build_shell_tool "$i"
+				kext_type=`echo $XCODE_CONFIG|tr A-Z a-z`
+				res_list[i+2]="$RES_DIR/Kext_builds/$pkg_name/build/$kext_type"
 				;;
 		esac
 	done
 }
 
 copy_resources() {
-	echo -e "\n${GREEN}Moving resources into place${NC}" >$(tty)
-	for (( i = 0; i < ${#res_list[@]} ; i+=3 )); do
-		pkg_name=`echo ${res_list[i+1]}|rev|cut -f 1 -d /|rev`
-		dest=${res_list[i+2]}
-		case `echo ${res_list[i]}|rev|cut -f 1 -d .|rev` in
-			"efi" )
-				echo -e -n "Copying ${res_list[i]} to $dest ... " >$(tty)
-				cp $RES_DIR/UDK/Build/$pkg_name/$AUDK_BUILD_DIR/X64/${res_list[i]} $BUILD_DIR/$dest
-				fin
-				;;
-			"kext" )
-				echo -e -n "Copying ${res_list[i]} to $dest ... " >$(tty)
-				cp -r $RES_DIR/Kext_builds/$pkg_name/build/$XCODE_CONFIG/${res_list[i]} $BUILD_DIR/$dest
-				fin
-				;;
-			"extras" )
-				pkg_name=`echo ${res_list[i]}|cut -f -2 -d .`
-				echo -e -n "Copying $pkg_name to $dest ... " >$(tty)
-				cp $BASE_DIR/extras/$pkg_name $BUILD_DIR/$dest
-				fin
-				;;
-		esac
+	msg "\n${GREEN}Moving resources into place${NC}\n"
+	for (( i = 0; i < ${#res_list[@]} ; i+=4 )); do
+		dest=${res_list[i+3]}
+		if [ "$dest" != "" ]; then
+			res_name=${res_list[i]}
+			srce=${res_list[i+2]}
+			msg "Copying $res_name to $dest ... "
+			mkdir -p $BUILD_DIR/$dest && cp -r $srce/$res_name $_
+			fin
+		fi
 	done
-	echo -e -n "Copying $CONFIG_PLIST to EFI/OC ... " >$(tty)
-	cp $BASE_DIR/$CONFIG_PLIST $BUILD_DIR/OC
-	fin
 }
 
 config_changed() {
 	cp $RES_DIR/UDK/OpenCorePkg/Docs/Sample.plist $BASE_DIR/Docs/Sample.plist
 	cp $RES_DIR/UDK/OpenCorePkg/Docs/SampleFull.plist $BASE_DIR/Docs/SampleFull.plist
-	echo -e "\n${YELLOW}WARNING:${NC} Sample.plist & SampleFull.plist have been updated\n${RED}Make sure${NC} $BASE_DIR/$CONFIG_PLIST ${RED}is up to date${NC}.\nRun the tool again if you make any changes." >$(tty)
+	msg "\n${YELLOW}WARNING:${NC} Samplei$1.plist has been updated\n${YELLOW}!!!${NC} Make sure$ $BASE_DIR/$CONFIG_PLIST is up to date${NC}.\nRun the tool again if you make any changes.\n"
 }
 
 
 check_if_Sample_plist_updated() {
-	echo -e -n "\nChecking if config.plist format has changed ... " >$(tty)
+	msg "\nChecking if config.plist format has changed ... "
 	cmp --silent $RES_DIR/UDK/OpenCorePkg/Docs/Sample.plist $BASE_DIR/Docs/Sample.plist||config_changed
-	cmp --silent $RES_DIR/UDK/OpenCorePkg/Docs/SampleFull.plist $BASE_DIR/Docs/SampleFull.plist||config_changed
+	cmp --silent $RES_DIR/UDK/OpenCorePkg/Docs/SampleFull.plist $BASE_DIR/Docs/SampleFull.plist||config_changed "Full"
 	fin
 }
 
 build_vault() {
 	use_vault=`/usr/libexec/PlistBuddy -c "print :Misc:Security:RequireVault" $BASE_DIR/$CONFIG_PLIST`||use_vault="false"
 	if [ "$use_vault" == "true" ]; then
-		echo -e -n "\nBuilding vault files for $BUILD_DIR ... " >$(tty)
+		msg "\nBuilding vault files for $BUILD_DIR ... "
 		cd $BUILD_DIR/OC
 		if ls vault* 1> /dev/null 2>&1; then
 			rm vault.*
@@ -265,31 +258,27 @@ build_vault() {
 		dd of=OpenCore.efi if=vault.pub bs=1 seek=$off count=520 conv=notrunc
 		rm vault.pub
 		fin
-#	else
-#		echo -e "\nRequireVault not set in $BASE_DIR/$CONFIG_PLIST\nskipping vault" >$(tty)
 	fi
 }
 
 set_build_type() {
 	case $ARG2 in
 		d?(ebug) )
-			echo -e "\n${GREEN}Setting up ${YELLOW}DEBUG${GREEN} environment${NC}" >$(tty)
 			XCODE_CONFIG="Debug"
-			AUDK_CONFIG="DEBUG"
 			;;
 		r?(elease) )
-			echo -e "\n${GREEN}Setting up ${YELLOW}RELEASE${GREEN} environment${NC}" >$(tty)
 			XCODE_CONFIG="Release"
-			AUDK_CONFIG="RELEASE"
 			;;
 		*)
 			echo -e "usage: (b)uild (r)elease, (b)uild (d)ebug" >$(tty)
 			exit 1
 			;;
 	esac
+	AUDK_CONFIG=`echo $XCODE_CONFIG|tr a-z A-Z`
 	BUILD_DIR="$BASE_DIR/$AUDK_CONFIG/EFI"
 	CONFIG_PLIST="$AUDK_CONFIG/config.plist"
 	AUDK_BUILD_DIR="$AUDK_CONFIG""_XCODE5"
+	echo -e "\n${GREEN}Setting up ${YELLOW}$AUDK_CONFIG${GREEN} environment${NC}" >$(tty)
 }
 
 add_drivers_res_list() {
@@ -301,15 +290,15 @@ add_drivers_res_list() {
 		if [ "$Driver" != "" ]; then
 			git_url=`/usr/libexec/PlistBuddy -c "print :$Driver" $BASE_DIR/Docs/repo.plist`||git_url=""
 			if [ "$git_url" != "" ]; then
-				res_list+=("$Driver" "$git_url" "OC/Drivers")
+				res_list+=("$Driver" "$git_url" "" "OC/Drivers")
 			elif [ -f "$BASE_DIR/extras/$Driver" ]; then
-				res_list+=("$Driver.extras" "extras/dummyPkg" "OC/Drivers")
+				res_list+=("$Driver.extras" "extras/dummyPkg" "$BASE_DIR/extras" "OC/Drivers")
 			else
-				echo -e "\n${RED}ERROR:${NC} $Driver - repo was not found in Docs/repo.plist or extras" >$(tty)
+				msg "\n${RED}ERROR:${NC} $Driver - repo was not found in Docs/repo.plist or extras\n"
 				exit 1
 			fi
 		fi
-	let "count += 1"
+		let "count += 1"
 	done
 }
 
@@ -324,14 +313,36 @@ add_kexts_res_list() {
 			if [ "$Enabled" == "true" ]; then
 				git_url=`/usr/libexec/PlistBuddy -c "print :$BundlePath" $BASE_DIR/Docs/repo.plist`||git_url=""
 				if [ "$git_url" != "" ]; then
-					res_list+=("$BundlePath" "$git_url" "OC/Kexts")
+					res_list+=("$BundlePath" "$git_url" "" "OC/Kexts")
 				else
-					echo -e "\n${RED}ERROR:${NC} $BundlePath - repo was not found in Docs/repo.plist" >$(tty)
+					msg "\n${RED}ERROR:${NC} $BundlePath - repo was not found in Docs/repo.plist\n"
 					exit 1
 				fi
 			fi
 		fi
-	let "count += 1"
+		let "count += 1"
+	done
+}
+
+add_tools_res_list() {
+	count=0
+	Path="start"
+	while [ "$Path" != "" ]
+	do
+		Path=`/usr/libexec/PlistBuddy -c "print :Misc:Tools:$count:Path" $BASE_DIR/$CONFIG_PLIST`||Path=""
+		Enabled=`/usr/libexec/PlistBuddy -c "print :Misc:Tools:$count:Enabled" $BASE_DIR/$CONFIG_PLIST`||Enabled=""
+		if [ "$Enabled" == "true" ]; then
+			git_url=`/usr/libexec/PlistBuddy -c "print :$Path" $BASE_DIR/Docs/repo.plist`||git_url=""
+			if [ "$git_url" != "" ]; then
+				res_list+=("$Path" "$git_url" "" "OC/Tools")
+			elif [ -f "$BASE_DIR/extras/$Path" ]; then
+					res_list+=("$Path" "extras/dummyPkg" "$BASE_DIR/extras" "OC/Tools")
+			else
+				msg "\n${RED}ERROR:${NC} $Name - repo was not found\n"
+				exit 1
+			fi
+		fi
+		let "count += 1"
 	done
 }
 
@@ -341,7 +352,7 @@ case $ARG1 in
 		set_build_type
 		;;
 	c?(opy) ) #copy from extras first if exists, else build from repo
-		echo -e "\n${YELLOW}copy mode does not exist${NC}"
+		msg "\n${YELLOW}copy mode not implemented yet${NC}\n"
 		exit 0
 		;;
 	u?(pdate) )
@@ -349,7 +360,7 @@ case $ARG1 in
 		exit 0
 		;;
 	*)
-		echo -e "usage: OpenCore-tool.sh\t(b)uild (r)elease,\n\t\t\t(b)uild (d)ebug,\n\t\t\t(u)pdate" >$(tty)
+		msg "\nusage: OpenCore-tool.sh\tcommand <option>\n\n\t\t\t(b)uild <(d)ebug,(r)elease>\n\t\t\t(c)opy  <(d)ebug,(r)elease>\n\t\t\t(u)pdate\n"
 		exit 0
 		;;
 esac
@@ -361,16 +372,17 @@ exec 2>&1
 
 check_requirements
 
-set_up_dirs
+set_up_dest_dirs
 
+init_res_list
 add_drivers_res_list
 add_kexts_res_list
+add_tools_res_list
 
 #check_for_updates
-echo -e "\n${RED}auto update check currently disabled${NC}\nrun ${YELLOW}./OpenCore-tool.sh update${NC} to update" >$(tty)
+msg "\n${RED}auto update check currently disabled${NC}\nrun ${YELLOW}./OpenCore-tool.sh update${NC} to update\n"
 
 build_resources
-
 copy_resources
 
 check_if_Sample_plist_updated
@@ -379,8 +391,4 @@ build_vault
 
 exec 1>&6 6>&- 2>&1 #stop logfile
 
-echo -e "\n${GREEN}Finished building ${YELLOW}$BUILD_DIR${NC}"
-
-#echo -e "\n${GREEN}Any git updates will appear below ...${NC}"
-#cat $BASE_DIR/$LOGFILE|grep "From http"|| echo -e "No git updates were found"
-#fin
+msg "\n${GREEN}Finished building ${YELLOW}$BUILD_DIR${NC}\n"
