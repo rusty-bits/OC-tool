6dfd271761c825db8eb1b9909c41affaa8794361
rusty-bits <rusty_bits@aol.com>
2020-03-05 18:57:56 -0500

    3.5(050320)

--- .tool-files/parsePlistMod.sh
+++ .tool-files/parsePlistMod.sh
@@ -37,10 +37,10 @@ found_split() {
 }
 
 found_empty_array() {
-	echo "Line number $line_num" >> errors.txt
-	echo "Found empty <array> in $section $sub1 $sub2 $item $key" >> errors.txt
-	echo "This may not be an issue for certain sections" >> errors.txt
-	echo "" >> errors.txt
+	echo "Line number $line_num" >> errors.minor.txt
+	echo "Found empty <array> in $section $sub1 $sub2 $item $key" >> errors.minor.txt
+	echo "This may not be an issue for certain sections" >> errors.minor.txt
+	echo "" >> errors.minor.txt
 }
 
 found_empty_dict() {
